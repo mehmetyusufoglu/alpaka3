778bc57eafed3bbcfb415bf9fe7076bd3ca66e51
mehmet yusufoglu <mehmetyusufoglu01@gmail.com>
2025-11-12 18:19:35 +0100

    fix 4

--- example/tensor/collectiveFFT/src/tensorCollectiveFFT.cpp
+++ example/tensor/collectiveFFT/src/tensorCollectiveFFT.cpp
@@ -21,6 +21,7 @@
 #include "collectiveFft/verification.hpp"
 
 #include <alpaka/alpaka.hpp>
+#include <alpaka/onHost/copy.hpp>
 #include <alpaka/onHost/example/executors.hpp>
 #include <alpaka/onHost/executeForEach.hpp>
 #include <alpaka/onHost/interface.hpp>
@@ -381,7 +382,7 @@ namespace
             spectralTensor.markHostModified();
 
             spectralTensor.ensureOnDevice(device, queue);
-            alpaka::onHost::wait(queue);
+            alpaka::onHost::copy(queue, device, spectralTensor);
             auto& deviceBuffer = spectralTensor.deviceBuffer(device, queue);
             auto* devicePtr = alpaka::onHost::data(deviceBuffer);
 
